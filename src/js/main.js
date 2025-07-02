bb6b8695be6230a8f23422bf62e963df5509e00e
Otabekcdr <boltaboyevotabek207@gmail.com>
2025-07-03 01:47:20 +0500

    almost done

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+
+// header dropdown btn
+function toggleDropdown() {
+    const dropdown = document.getElementById("dropdown")
+    dropdown.classList.toggle("hidden")
+}
+
+document.addEventListener("click", function (event) {
+    const dropdown = document.getElementById("dropdown")
+    const servicesLink = event.target.closest('a[onclick="toggleDropdown()"]')
+
+    if (!servicesLink && !dropdown.contains(event.target)) {
+        dropdown.classList.add("hidden")
+    }
+})
+
+// header menu btn
+const menuBtn = document.getElementById("menu-btn")
+const menu = document.getElementById("mobile-menu")
+
+let isOpen = false
+
+menuBtn.addEventListener("click", () => {
+    isOpen = !isOpen
+
+    menuBtn.classList.toggle("fa-bars", !isOpen)
+    menuBtn.classList.toggle("fa-xmark", isOpen) 
+
+    if (isOpen) {
+        menu.classList.remove("-left-[100%]")
+        menu.classList.add("left-0")
+    } else {
+        menu.classList.remove("left-0")
+        menu.classList.add("-left-[100%]")
+    }
+})
---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+const showcaseSwiper = new Swiper(".showcaseSwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
